09c956c1ac44afe3af57a2b492b8c6d9a6b88458
Mateusz Pal <pal.mateusz@gmail.com>
2020-09-23 11:14:07 +0200

    temperature service mock implementation

--- src/main/java/learn/service/sensor/TemperatureServiceMock.java
+++ src/main/java/learn/service/sensor/TemperatureServiceMock.java
@@ -1,49 +1,89 @@
 package learn.service.sensor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import learn.exception.ServiceException;
+import learn.service.MessengerImpl;
 import learn.service.MyService;
 import learn.service.messenger.Messenger;
+import learn.service.messenger.Topic;
 
 public class TemperatureServiceMock implements MyService, Runnable {
 	private static final Integer PROBABILITY = 70;
-	private int temperature = -1;
+	private AtomicInteger temperature = new AtomicInteger(-1);
 	private Messenger messenger;
+	private Random random;
+	private boolean running = false;
+	private String name;
 
-	public void TemperatureServiceMock() {
+	public TemperatureServiceMock(String name) {
+		messenger = MessengerImpl.getInstance();
+		random = new Random();
+		this.name = name;
+	}
 
+	public TemperatureServiceMock() {
+		messenger = MessengerImpl.getInstance();
+		random = new Random();
 	}
 
 	@Override
 	public void run() {
-		try {
-			Thread.sleep(1000);
-			if (shouldChange()) {
-				int offset = calculateOffset();
-				temperature += offset;
+		while (running) {
+			try {
+				sendMessageAndWait();
+				updateValue();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				Thread.currentThread()
+					.interrupt();
 			}
-
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			throw new ServiceException();
 		}
 	}
 
 	@Override
-	public void startService() throws ServiceException {
+	public void startService() {
 		try {
+			running = true;
 			run();
+			temperature.set(23);
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new ServiceException();
 		}
 	}
 
+	@Override
+	public void stopService() {
+		running = false;
+	}
+
+	private void sendMessageAndWait() throws InterruptedException {
+		String message = generateMessage();
+		messenger.publish(Topic.TEMPERATURE, message);
+		Thread.sleep(1000);
+	}
+
+	private String generateMessage() {
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss ");
+		LocalDateTime now = LocalDateTime.now();
+
+		return dateTimeFormatter.format(now) + name + ": " + temperature;
+	}
+
+	private void updateValue() {
+		if (shouldChange()) {
+			int offset = calculateOffset();
+			temperature.getAndAdd(offset);
+		}
+	}
+
 	private boolean shouldChange() {
 		boolean change = false;
 
-		Random random = new Random();
 		int chance = random.nextInt(100);
 		if (chance > PROBABILITY) {
 			change = true;
@@ -52,7 +92,8 @@ public class TemperatureServiceMock implements MyService, Runnable {
 	}
 
 	private int calculateOffset() {
-		return 0;
+		int randomNumber = random.nextInt(2);
+		return randomNumber - random.nextInt(2);
 	}
 
 }
